d6eb3ed547a62dd8824c3f13049d60cb2b0e1453
Lucretia Maswabela <maswabeladipuo@gmail.com>
2022-08-30 18:29:02 +0200

    first commit
    Changed plane to automatically move forward

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed=5.0f;
-    public float rotationSpeed = 5.0f;
+    public float rotationSpeed = 10.0f;
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -21,7 +21,7 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward *Time.deltaTime * speed*verticalInput);
+        transform.Translate(Vector3.forward *Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime* verticalInput);
